ccfa38ab949b1b7d2ad656dd753a4278246d5d68
Kent Seneres <kent.seneres@gmail.com>
2017-12-04 23:02:48 -0500

    Add web stuff

---
+++ web/database_manager.php
@@ -0,0 +1,74 @@
+<?php
+
+class DatabaseManager extends SQLite3 {
+	function __construct() {
+		$this->open("users.db"); 
+	}
+
+	function init() {
+		$sql = "CREATE TABLE USERS(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			win_count INTEGER DEFAULT 0,
+			loss_count INTEGER DEFAULT 0
+			);";
+
+		return $this->exec($sql); 
+	}
+
+	function dumpUsers() {
+		$sql = "SELECT * FROM USERS"; 
+		return $this->query($sql); 
+	}
+
+	function updateBoard($winner_id, $loser_id) {
+		$sql = "UPDATE USERS SET win_count = win_count + 1 WHERE id = $winner_id;
+			UPDATE USERS SET loss_count = loss_count + 1 WHERE id = $loser_id"; 
+		return $this->exec($sql); 
+	}
+
+	function insertUser($name) {
+		$sql = "INSERT INTO USERS VALUES (null, '$name', 0, 0)"; 
+		return $this->exec($sql); 
+	}
+
+	function updateUserName($id, $new_name) {
+		$sql = "UPDATE USERS SET name = '$new_name' WHERE id = $id"; 
+		return $this->exec($sql); 
+	}
+
+	function updateUserWinCount($id, $new_win) {
+		$sql = "UPDATE USERS SET win_count = '$new_win' WHERE id = $id"; 
+		return $this->exec($sql); 
+	}
+
+	function updateUserLossCount($id, $new_loss) {
+		$sql = "UPDATE USERS SET loss_count = '$new_loss' WHERE id = $id"; 
+		return $this->exec($sql); 
+	}
+
+	function deleteUser($id) {
+		$sql = "DELETE FROM USERS WHERE id = $id"; 
+		return $this->exec($sql); 
+	}
+}
+
+/*
+$db = new DatabaseManager(); 
+if (!$db) {
+	echo $db->lastErrorMsg(); 
+} else {
+	echo "Opened database successfully\n"; 
+}
+
+if (!$db->init()) {
+	echo $db->lastErrorMsg(); 
+} else {
+	echo "Table created successfully\n"; 
+}
+
+$db->close(); 
+*/
+
+
+?>
---
+++ web/update.php
@@ -0,0 +1,42 @@
+<?php
+
+	if ($_POST["data"] || $_POST["wid"] || $_POST["lid"] || $_POST["name"]) {
+
+		include 'database_manager.php';
+
+		$db = new DatabaseManager(); 
+		if (!$db) {
+			echo $db->lastErrorMsg(); 
+			return; 
+		}
+		$redirect = false; 
+
+		if ($_POST["data"]) {
+			// The expected data is in the format of W0L0W1L1...WnLn
+			// Where Wi and Li for 0 <= i <= n are the winner and loser ids
+			$ids = str_split($_POST["data"]); 
+
+			// Go through each win-loss pair and update the database
+			for ($i = 0; $i < sizeof($ids); $i += 2) {
+				if (!$db->updateBoard($ids[$i], $ids[$i + 1])) {
+					echo $db->lastErrormsg(); 
+				} 
+			}
+		} 
+		else if ($_POST["wid"] && $_POST["lid"]) {
+			$db->updateBoard($_POST["wid"], $_POST["lid"]);
+			$redirect = true; 
+		}
+		else if ($_POST["name"]) {
+			$db->insertUser($_POST["name"]);
+			$redirect = true; 
+		}
+
+		$db->close();
+
+		if ($redirect) {
+			header("location: index.php");
+		}
+	}
+
+?>
